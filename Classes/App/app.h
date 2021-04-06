53052c7ca0bc1b3c9bfc964a2466efc3a9a13483
Aravix <aravixdev@gmail.com>
2021-04-06 23:17:41 +0200

    Changing around App.h variables

--- Classes/App/app.h
+++ Classes/App/app.h
@@ -5,17 +5,23 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <Utils/enums.h>
+#include <Classes/Texture/texture.h>
 
 class App {
 public:
-	App();
-	~App();
-	SDL_Texture* icon;
-	std::string name;
-	Xynergy_AppType appType;
-	std::pair<int, int> coordinates;
-private:
+	// The icon. Use the Texture::loadImage() function.
+	Texture icon;
 
+	// The name. Use the Texture::loadText() function.
+	Texture name;
+
+	// Determines the expected logic when you click on it.
+	Xynergy_AppType appType;
+
+	// If false it won't appear on the dashboard.
+	// Coordinates will also be ignored.
+	bool showOnDashboard;
+	SDL_Rect coordinates;
 };
 
 #endif
