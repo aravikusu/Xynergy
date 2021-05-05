2c42b83df134439b726b457e6c84cccf0201e82b
Aravix <aravixdev@gmail.com>
2021-05-06 01:12:56 +0200

    UserSettings

---
+++ Classes/Settings/usersettings.h
@@ -0,0 +1,42 @@
+#ifndef USERSETTINGS_H
+#define USERSETTINGS_H
+
+#define MINI_CASE_SENSITIVE
+#include <string>
+#include <Utils/libs/ini.h>
+#include <SDL.h>
+#include <Utils/enums.h>
+#include <Utils/libs/ini.h>
+
+/// <summary>
+/// Aka the "save file". Also one of the few functions that is completely public.
+/// </summary>
+class UserSettings {
+public:
+	/// <summary>
+	/// Fetches the settings file for the requested user and reads all the data.
+	/// </summary>
+	void readUserSettings(std::string username);
+
+	/// <summary>
+	/// Save all current settings to the requested user.
+	/// </summary>
+	/// <param name="username"></param>
+	void writeUserSettings();
+
+	/// <summary>
+	/// Clears out all current settings in memory.
+	/// </summary>
+	void clearSettings();
+
+	std::string TaskbarOrientationToString(Xynergy_TaskbarOrientation orientation);
+	Xynergy_TaskbarOrientation stringToTaskbarOrientation(std::string orientation);
+
+	std::string name;
+	std::string displayName;
+	std::string currentWallpaper;
+	std::string currentTheme;
+	Xynergy_TaskbarOrientation curentOrientation;
+};
+
+#endif
